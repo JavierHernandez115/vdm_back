1b7d5fb6e6673e580fe78a8b98673bd1437b2aa1
JavierHernandez115 <66996054+JavierHernandez115@users.noreply.github.com>
2024-11-30 12:19:33 -0600

    Update settings.py

--- VDM_Servidor/settings.py
+++ VDM_Servidor/settings.py
@@ -143,10 +143,7 @@ STORAGES = {
 
 CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-8f9d.up.railway.app']
 
-
-CORS_ALLOWED_ORIGINS = ['https://web-production-8f9d.up.railway.app','http://localhost:4200']
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_WHITELIST=['https://web-production-8f9d.up.railway.app']
 CORS_ALLOW_HEADERS = [
     "accept",
     "accept-encoding",
