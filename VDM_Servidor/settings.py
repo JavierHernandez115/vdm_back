067051f117d4164ed3c70a366f20b25d9b32c2ac
JavierHernandez115 <66996054+JavierHernandez115@users.noreply.github.com>
2024-11-30 04:55:57 -0600

    Solve white Noise

--- VDM_Servidor/settings.py
+++ VDM_Servidor/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'whitenoise.runserver_nostatc',
+    'whitenoise.runserver_nostatic',
     'rest_framework',
     'Servidor', 
 ]
