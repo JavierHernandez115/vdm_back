101b01a7429075fda1f43dc44c4348078a938734
JavierHernandez115 <66996054+JavierHernandez115@users.noreply.github.com>
2024-11-30 04:42:23 -0600

    Allow Host

--- VDM_Servidor/settings.py
+++ VDM_Servidor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&al&dythfejfo8iwn!$8+72c+pai0&j=57(8s@_@%bpv!3-e3f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','web-production-8f9d.up.railway.app']
 
 
 # Application definition
