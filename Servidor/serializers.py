48f1d98d8b7a90de0aa4c264671072249808f736
JavierHernandez115 <66996054+JavierHernandez115@users.noreply.github.com>
2024-12-03 23:26:26 -0600

    Solve Serlizers

--- Servidor/serializers.py
+++ Servidor/serializers.py
@@ -7,36 +7,44 @@ class EmpleadoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class AsistenciaSerializer(serializers.ModelSerializer):
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
     class Meta:
         model = Asistencia
         fields = '__all__'
 
 class VacacionSerializer(serializers.ModelSerializer):
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
     class Meta:
         model = Vacacion
         fields = '__all__'
 
 class VacacionTomadaSerializer(serializers.ModelSerializer):
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
     class Meta:
         model = VacacionTomada
         fields = '__all__'
 
 class SalarioSerializer(serializers.ModelSerializer):
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
     class Meta:
         model = Salario
         fields = '__all__'
 
 class PrestamoSerializer(serializers.ModelSerializer):
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
     class Meta:
+
         model = Prestamo
         fields = '__all__'
 
 class AbonoSerializer(serializers.ModelSerializer):
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
     class Meta:
         model = Abono
         fields = '__all__'
 
 class PagoSerializer(serializers.ModelSerializer):
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
     class Meta:
         model = Pago
         fields = '__all__'
